b80292c6837e7cb7e13f4583364fd3c96131f144
Brad Alexander <bralexander@live.com>
2021-01-29 23:14:59 -0600

    updated app.py paths for more html files

--- app.py
+++ app.py
@@ -13,19 +13,35 @@ def main_page():
 def embedded():
 	return render_template('embedded.html')
 
+@app.route('/loadscript')
+def loadscript():
+	return render_template('loadscript.html')
 
 @app.route('/compvis')
 def compvis():
-	return render_template('compvis.html')
+	return render_template('compvis.html', pdb_file='6ij6.pdb', pdb_csv='6ij6.csv')
 
 
 @app.route('/compvis/<pdb_id>')
-def dynamic_compvis(pdb_id='6ij6'):
+def dynamic_compvis(pdb_id):
 	
 	pdb_id   = pdb_id.lower()
 	pdb_file = pdb_id + ".pdb"
 	pdb_csv  = pdb_id + ".csv"
 	return render_template('compvis.html', pdb_file=pdb_file, pdb_csv=pdb_csv)
+
+@app.route('/compvis2')
+def compvis2():
+	return render_template('compvis2.html', pdb_file='6ij6.pdb', pdb_csv='6ij6.csv')
+
+
+@app.route('/compvis2/<pdb_id>')
+def dynamic_compvis2(pdb_id):
+	
+	pdb_id   = pdb_id.lower()
+	pdb_file = pdb_id + ".pdb"
+	pdb_csv  = pdb_id + ".csv"
+	return render_template('compvis2.html', pdb_file=pdb_file, pdb_csv=pdb_csv)
 
 
 if __name__=="__main__":
